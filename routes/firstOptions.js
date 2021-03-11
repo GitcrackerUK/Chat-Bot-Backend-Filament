24fc713bdcc5b908ec9b1ab8b473ad7468af08b7
gitcrackerUK <pfsiwek@gmail.com>
2021-03-11 22:50:16 +0000

    comments

--- routes/firstOptions.js
+++ routes/firstOptions.js
@@ -4,9 +4,11 @@ const opt2 = require('../store').opt2;
 const opt3 = require('../store').opt3;
 const opt4 = require('../store').opt4;
 const opt5 = require('../store').opt5;
-
+// imported objects with further options for user to choose
 router.route('/').post((req, res) => {
     let response = {};
+    // conditional used to respond with right set of options accordingly to choose option by user.
+    // can be use switch to assign right set of options to "response"
     let choiceNum = req.body.answer;
     if (choiceNum === 1) {
         response = opt1;
