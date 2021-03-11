dacaa685658d7ab215977bb5a68e73f2575464c3
gitcrackerUK <pfsiwek@gmail.com>
2021-03-11 22:43:17 +0000

    added options 3,4,5 to firstOptions branch

--- routes/firstOptions.js
+++ routes/firstOptions.js
@@ -1,10 +1,25 @@
 const router = require('express').Router();
 const opt1 = require('../store').opt1;
 const opt2 = require('../store').opt2;
+const opt3 = require('../store').opt3;
+const opt4 = require('../store').opt4;
+const opt5 = require('../store').opt5;
 
 router.route('/').post((req, res) => {
-    console.log(req.body);
-    res.send(opt2);
+    let response = {};
+    let choiceNum = req.body.answer;
+    if (choiceNum === 1) {
+        response = opt1;
+    } else if (choiceNum === 2) {
+        response = opt2;
+    } else if (choiceNum === 3) {
+        response = opt3;
+    } else if (choiceNum === 4) {
+        response = opt4;
+    } else if (choiceNum === 5) {
+        response = opt5;
+    }
+    res.send(response);
 });
 
 module.exports = router;
