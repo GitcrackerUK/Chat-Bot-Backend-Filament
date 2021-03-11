26bdd9df95bbc6af6343d449f688a4506aee4fec
gitcrackerUK <pfsiwek@gmail.com>
2021-03-11 21:36:55 +0000

    routes Welcome & firstOptions

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const bodyParser = require('body-parser'); // body parser is not needed because express has it built in
+const firstOptions = require('./routes/firstOptions');
+const Welcome = require('./routes/welcome');
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -7,9 +8,8 @@ const port = 3000;
 app.use(cors());
 app.use(express.json()); // it is used instead of bodyParser app.use(bodyParser.json())
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+app.use('/', Welcome); // base route for welcome text and question for further branching
+app.use('/firstOptions', firstOptions); // route triggered to handle first set of options
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
